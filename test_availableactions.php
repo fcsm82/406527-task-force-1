c0b61696e36f615e02a8da3ff325166a4f409185
Alexey Ivanov <alexey.ivanov.private@gmail.com>
2019-10-29 11:17:01 -0400

    Исправлено сравнение передаваемого действия и статуса

--- test_availableactions.php
+++ test_availableactions.php
@@ -4,8 +4,8 @@ spl_autoload_register ();
 
 $action = new AvailableActions ();
 
-assert($action->getNextStatus(AvailableActions::ACTION_CANCEL) == AvailableActions::STATUS_CANCELED, 'canceled');
-assert($action->getNextStatus(AvailableActions::ACTION_RESPOND) == null, 'canceled');
-assert($action->getNextStatus(AvailableActions::ACTION_START_PROCESS) == AvailableActions::STATUS_NEW, 'in process');
-assert($action->getNextStatus(AvailableActions::ACTION_COMPLETE) == AvailableActions::STATUS_COMPLETED, 'completed');
-assert($action->getNextStatus(AvailableActions::ACTION_REFUSE) == AvailableActions::STATUS_FAILED, 'failed');
+assert($action->getNextStatus(AvailableActions::ACTION_CANCEL) === AvailableActions::STATUS_CANCELED, 'При передачи действия cancel возвращается статус canceled');
+assert($action->getNextStatus(AvailableActions::ACTION_RESPOND) === null, 'При передачи действия respond возвращается статус null');
+assert($action->getNextStatus(AvailableActions::ACTION_START_PROCESS) === AvailableActions::STATUS_NEW, 'При передачи действия start process возвращается статус new');
+assert($action->getNextStatus(AvailableActions::ACTION_COMPLETE) === AvailableActions::STATUS_COMPLETED, 'При передачи действия complete возвращается статус completed');
+assert($action->getNextStatus(AvailableActions::ACTION_REFUSE) === AvailableActions::STATUS_FAILED, 'При передачи действия refuse возвращается статус failed');
