f143e69e8e1c80d8f3c4da3dfa207818a63e57e6
Alexey Ivanov <alexey.ivanov.private@gmail.com>
2019-10-30 10:41:59 -0400

    Исправлено тестирование класса

--- test_availableactions.php
+++ test_availableactions.php
@@ -1,11 +1,12 @@
 <?php
 
-spl_autoload_register ();
+spl_autoload_register();
 
-$action = new AvailableActions ();
+$action = new AvailableActions;
 
+assert($action->getNextStatus(AvailableActions::ACTION_CREATE) === AvailableActions::STATUS_NEW, 'При передачи действия create возвращается статус new');
 assert($action->getNextStatus(AvailableActions::ACTION_CANCEL) === AvailableActions::STATUS_CANCELED, 'При передачи действия cancel возвращается статус canceled');
 assert($action->getNextStatus(AvailableActions::ACTION_RESPOND) === null, 'При передачи действия respond возвращается статус null');
-assert($action->getNextStatus(AvailableActions::ACTION_START_PROCESS) === AvailableActions::STATUS_NEW, 'При передачи действия start process возвращается статус new');
+assert($action->getNextStatus(AvailableActions::ACTION_SELECT_PERFORMER) === AvailableActions::STATUS_IN_PROCESS, 'При передачи действия select performer возвращается статус in process');
 assert($action->getNextStatus(AvailableActions::ACTION_COMPLETE) === AvailableActions::STATUS_COMPLETED, 'При передачи действия complete возвращается статус completed');
 assert($action->getNextStatus(AvailableActions::ACTION_REFUSE) === AvailableActions::STATUS_FAILED, 'При передачи действия refuse возвращается статус failed');
